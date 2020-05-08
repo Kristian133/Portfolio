d39e6a42bb591a601b7c82402725129a046cb8d7
Kristian133 <56347165+Kristian133@users.noreply.github.com>
2020-05-08 17:43:07 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-content>
+        <v-container fluid>
+          <router-view />
+        </v-container>
+      </v-content>
+    </v-app>
+  </div>
+</template>
+
+<style lang="scss">
+.container {
+  padding: 0 !important;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
